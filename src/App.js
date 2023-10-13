b14ca9718459937b9f9b1365e673f4dda942accf
Viral-Sachde <vsachde74@gmail.com>
2023-10-13 16:37:05 +0530

    my comment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import Regulartimer from './Regulartimer';
+import Shorttimer from './Shorttimer';
+import Longtimer from './Longtimer';
 function App() {
+
+  const [activeComponent, setActiveComponent] = useState();
+  const handleButtonClick = (component) => {
+    console.log(`Clicked on button for ${component}`);
+    setActiveComponent(component);
+  };
+
+  console.log('Active Component:', activeComponent);
+  document.title = 'Minimal Pomo Timer';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <body class="flex items-center justify-center min-h-screen m-0 h-full flex-auto bg-white dark:bg-gray-900">
+        <main class="text-center">
+          <div class="mt-3 flex items-center justify-center gap-x-6">
+            <button class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold border border-gray-900 hover:border-none hover:bg-gray-900 hover:text-white  " onClick={() => handleButtonClick('First')}>Pomo</button>
+            <button class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold border border-gray-900 hover:border-none hover:bg-gray-900 hover:text-white " onClick={() => handleButtonClick('Second')}>Short Break</button>
+            <button class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold  border border-gray-900 hover:border-none hover:bg-gray-900 hover:text-white " onClick={() => handleButtonClick('Third')}>Long Break</button>
+          </div>   {activeComponent === 'First' && <Regulartimer />}
+          {activeComponent === 'Second' && <Shorttimer />}
+          {activeComponent === 'Third' && <Longtimer />}
+          <h1 class="text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl"> </h1>
+          <div class="mt-3 flex items-center justify-center gap-x-6">
+            {/* <a href="#" class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold  border border-gray-900  hover:border-none hover:bg-gray-900 hover:text-white" onClick={addComponent}>{isActive ? 'Pause' : 'Start'}</a>
+            <a href="#" class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold border border-gray-900  hover:border-none hover:bg-gray-900 hover:text-white " onClick={handleReset}>Reset</a>
+            <a href="#" class="text-sm font-semibold text-gray-900 hover:underline ">Contact support <span aria-hidden="true">&rarr;</span></a> */}
+          </div>
+          <div className="fixed bottom-0 right-0 p-2 text-gray-900 text-xs">
+            Viral Sachde
+          </div>
+        </main>
+      </body>
     </div>
   );
 }
+const formatTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+};
 
 export default App;
---
+++ src/Regulartimer.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { useEffect, useState } from 'react';
+
+const Regulartimer = () => {
+    const [time, setTime] = useState(1500); // Initial time in seconds (25 minutes)
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+        let timer;
+
+        if (isActive && time > 0) {
+            timer = setInterval(() => {
+                setTime((prevTime) => prevTime - 1);
+            }, 1000);
+        } else if (time === 0) {
+            // Timer reached 0, handle completion or reset here
+            handleTimerComplete();
+        }
+
+        return () => clearInterval(timer);
+
+    }, [isActive, time]);
+
+    const handleStartPause = () => {
+        setIsActive((prevIsActive) => !prevIsActive);
+    };
+
+    const handleReset = () => {
+        setIsActive(false);
+        setTime(1500);
+    };
+
+    const handleTimerComplete = () => {
+        setIsActive(false);
+        // You can add additional logic here for completion
+    };
+
+
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        if( minutes === 0 && seconds === 0 ){alert("You have completed a set, Take a break now!");}
+        document.title = String(minutes).padStart(2, '0') + ':' + String(remainingSeconds).padStart(2, '0');
+        return `${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
+    };
+
+    const updateTitle = () => {
+        const formattedTime = formatTime();
+        document.title = `Pomodoro Timer - ${formattedTime}`;
+    };
+    return (
+        <>
+            {updateTitle}
+         
+            <h1 class="text-6xl font-bold tracking-tight text-gray-900 sm:text-7xl"> {formatTime(time)}</h1>
+            <div class="mt-3 flex items-center justify-center gap-x-6">
+                <a href="#" class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold  border border-gray-900  hover:border-none hover:bg-gray-900 hover:text-white" onClick={handleStartPause}>{isActive ? 'Pause' : 'Start'}</a>
+                <a href="#" class="rounded-2xl text-gray-900  px-3.5 py-2.5 text-sm font-semibold border border-gray-900  hover:border-none hover:bg-gray-900 hover:text-white " onClick={handleReset}>Reset</a>
+                <a href="#" class="text-sm font-semibold text-gray-900 hover:underline ">Contact support <span aria-hidden="true">&rarr;</span></a>
+            </div></>
+    )
+}
+
+export default Regulartimer
